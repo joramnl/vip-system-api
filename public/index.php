d71f92b81778f094ae6345e81941437d83c40152
joramnl <uY2Yg5GtaDSKy5z>
2020-02-01 21:49:03 +0100

    Added error handler

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use Medoo\Medoo;
-use Monolog\Handler\FirePHPHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use \Psr\Http\Message\ServerRequestInterface as Request;
@@ -12,20 +11,39 @@ require '../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
 
-$app = new \Slim\App;
+$app = new \Slim\App([
+    "settings" => [
+        "displayErrorDetails" => true
+    ]
+]);
 
 $container = $app->getContainer();
 
 $container['logger'] = function($c) {
-    $logger = new Logger('my_logger');
-    $file_handler = new StreamHandler('../logs/app.log');
-    $firephp = new FirePHPHandler();
-    $logger->pushHandler($file_handler);
-    $logger->pushHandler($firephp);
+    $logger = new Logger('logger');
+
+    $logger->pushHandler(new StreamHandler('../logs/app.log'));
+
     return $logger;
 };
 
-$container['database'] = function () {
+$container['errorHandler'] = function ($container) {
+    return function (Request $request, Response $response, Exception $exception) use ($container) {
+        error_log($exception);
+        $container->logger->error("Error", [$exception]);
+
+        $error = [
+            "success" => false,
+            "error" => $exception->getMessage()
+        ];
+
+        return $response->withStatus(500)
+            ->withHeader('Content-Type', 'application/json')
+            ->write(json_encode($error));
+    };
+};
+
+$container['db'] = function () {
     return new Medoo([
         'database_type' => 'mysql',
         'database_name' => getenv("DB_NAME"),
@@ -35,12 +53,12 @@ $container['database'] = function () {
     ]);
 };
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    $this->logger->info('Hello ', array('username' => $name));
+$app->get('/', function (Request $request, Response $response, array $args) {
 
-    return $response;
+    $data = $this->db->select('account', ['id', 'name']);
+
+    return $response->write(json_encode($data));
+
 });
 
 $app->run();
