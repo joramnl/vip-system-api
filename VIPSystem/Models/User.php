70f6604486734df2d47c8e02a9e2a1987d45178a
joramnl <uY2Yg5GtaDSKy5z>
2020-02-18 21:22:43 +0100

    Updated user model

--- VIPSystem/Models/User.php
+++ VIPSystem/Models/User.php
@@ -7,21 +7,21 @@ namespace VIPSystem\Models;
 class User
 {
 
+    private $user_id;
     private $user_name;
     private $user_steamid;
     private $user_base64_image;
 
     /**
      * User constructor.
-     * @param $user_name
-     * @param $user_steamid
-     * @param $user_base64_image
+     * @param $data
      */
-    public function __construct($user_name, $user_steamid, $user_base64_image)
+    public function __construct($data)
     {
-        $this->user_name = $user_name;
-        $this->user_steamid = $user_steamid;
-        $this->user_base64_image = $user_base64_image;
+        $this->user_id = $data[0]["user_id"];
+        $this->user_name = $data[0]["user_name"];
+        $this->user_steamid = $data[0]["user_steamid"];
+        $this->user_base64_image = $data[0]["user_base64_image"];
     }
 
     /**
@@ -43,6 +43,14 @@ class User
     /**
      * @return mixed
      */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getUserBase64Image()
     {
         return $this->user_base64_image;
@@ -50,6 +58,7 @@ class User
 
     public function toArray() : array {
         return [
+            "user_id" => $this->user_id,
             "user_name" => $this->user_name,
             "user_steamid" => $this->user_steamid,
             "user_base64_image" => $this->user_base64_image,
